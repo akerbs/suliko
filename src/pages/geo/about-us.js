fe2c4f12521373a2a7938f74e8b2fd1d0750c849
Anatol Kerbs <anker2702@gmail.com>
2020-07-23 23:31:20 +0200

    230720_18

--- src/pages/geo/about-us.js
+++ src/pages/geo/about-us.js
@@ -13,6 +13,23 @@ export default () => {
 
   return (
     <Layout>
+      <iframe
+        title="music"
+        type="audio/mpeg"
+        style={{
+          zIndex: 100,
+          margin: 0,
+          padding: "7px 0px 0px 7px",
+          top: 0,
+          left: 0,
+          position: "absolute",
+        }}
+        width="30vw"
+        height="27"
+        scrolling="no"
+        frameborder="no"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/15237004&color=%23851a1d&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"
+      />
       <Container
         maxWidth="md"
         style={{
