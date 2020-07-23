d068106b5da45979e85e5f0d0dc3ba3465cbe2bc
Anatol Kerbs <anker2702@gmail.com>
2020-07-23 17:52:10 +0200

    230720_08

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,6 +17,7 @@ import bgImg1280 from "../images/bgImg1280.gif"
 import bgImg1600 from "../images/bgImg1600.gif"
 import ModalWindow from "./modalWindow"
 // import Scroll from "./scrollToTopBtn"
+import CardMedia from "@material-ui/core/CardMedia"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -188,9 +189,16 @@ const Layout = ({ location, children }, props) => {
     <Container className={classes.root}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <section>
+        <CardMedia
+          component="audio"
+          src="suliko.mp3"
+          controls
+          autoPlay
+          style={{ zIndex: 999 }}
+        ></CardMedia>
+        {/* <section>
           <audio src="suliko.mp3" controls autoPlay style={{ zIndex: 999 }} />
-        </section>
+        </section> */}
 
         <Container className={classes.content}>
           {/* <Scroll showBelow={250} /> */}
