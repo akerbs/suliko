704f97d49f2b5906b8125d101d3fc7d6b9cbf7bc
Anatol Kerbs <anker2702@gmail.com>
2020-07-25 11:58:19 +0200

    250720_02

--- src/components/audio-no-autoplay.js
+++ src/components/audio-no-autoplay.js
@@ -18,7 +18,7 @@ export default function AudioNoAutoplay() {
         height="27"
         scrolling="no"
         frameborder="no"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/15237004&color=%23851a1d&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/15237004&color=%23851a1d&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"
       />
     </>
   )
