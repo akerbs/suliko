d104ac461af7151212648a7e1e4737a7aa912f96
Anatol Kerbs <anker2702@gmail.com>
2020-07-25 15:14:59 +0200

    250720_12

--- src/pages/index-con.js
+++ src/pages/index-con.js
@@ -9,11 +9,10 @@ import { StaticQuery, graphql } from "gatsby"
 import sloganImg2 from "../images/slogan_eng2.png"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-const IndexContentfulPage = ({data}) => {
-   const firstRichContent = data.allContentfulRichContent.nodes[0]
+const IndexContentfulPage = ({ data }) => {
+  const firstRichContent = data.allContentfulRichContent.nodes[0]
   return (
     <Layout>
-   
       <AudioNoAutoPlay />
       <Container
         maxWidth="md"
@@ -46,17 +45,14 @@ const IndexContentfulPage = ({data}) => {
             borderRadius: 10,
           }}
         >
-                <Typography
+          <Typography
             variant="body1"
             align="center"
             style={{ lineHeight: 1.5 }}
             color="secondary"
           >
-                {documentToReactComponents(firstRichContent.text.json)}
-
-                
-           
-           <Link
+            {documentToReactComponents(firstRichContent.text.json)}
+            <Link
               to={"/Speisekarte.pdf"}
               target="_blank"
               style={{ textDecoration: "none" }}
@@ -64,26 +60,31 @@ const IndexContentfulPage = ({data}) => {
               <Button
                 variant="outlined"
                 color="secondary"
-                style={{marginRight: 10, marginTop: 10, width: 200}}
+                style={{ marginRight: 10, marginTop: 10, width: 200 }}
               >
                 Bestellmenükarte
               </Button>
             </Link>
-             <Link
+            <Link
               to={"/Weinkarte.pdf"}
               target="_blank"
               style={{ textDecoration: "none" }}
             >
-              <Button variant="outlined" color="secondary" style={{ marginRight: 10, marginTop: 10, width: 200}}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                style={{ marginRight: 10, marginTop: 10, width: 200 }}
+              >
                 Weinkarte
               </Button>
             </Link>
-             <br />
             <br />
-             <b> Unsere Öffnungszeiten: </b>
             <br />
-            Montag bis Donnerstag 12:00 – 15:00 Uhr, 
-            <br />17:00 – 22:00 Uhr
+            <b> Unsere Öffnungszeiten: </b>
+            <br />
+            Montag bis Donnerstag 12:00 – 15:00 Uhr,
+            <br />
+            17:00 – 22:00 Uhr
             <br />
             Freitag 12:00 – 15:00 Uhr, 17:00 – 00:00 Uhr
             <br />
@@ -92,7 +93,7 @@ const IndexContentfulPage = ({data}) => {
             Sonntag 15:00 – 22:00 Uhr
             <br />
             <br />
-             <b> Lieferungen & Abholungen:</b>
+            <b> Lieferungen & Abholungen:</b>
             <br />
             Mo. bis So. von 12.00 bis 18.00 Uhr und nach Vereinbarung.
             <br />
@@ -108,11 +109,7 @@ const IndexContentfulPage = ({data}) => {
             Sie erreichen uns telefonisch unter 040/49201953
             <br />
             Unter den Lieferdiensten finden Sie uns auf Lieferando
-         
-
-                </Typography>
-
-
+          </Typography>
         </Container>
       </Container>
     </Layout>
@@ -120,17 +117,17 @@ const IndexContentfulPage = ({data}) => {
 }
 
 export const query = graphql`
-            {
-  allContentfulRichContent {
-    nodes {
-      title
-      text {
-        json
+  {
+    allContentfulRichContent {
+      nodes {
+        title
+        text {
+          json
+        }
       }
     }
   }
-}
-            `
+`
 
 export default IndexContentfulPage
 
@@ -155,3 +152,58 @@ export default IndexContentfulPage
 // }
 // {data.markdownRemark.frontmatter.title}
 // {data.markdownRemark.html}
+
+// import React from "react"
+// import Layout from "../components/layout"
+// import Container from "@material-ui/core/Container"
+// import Typography from "@material-ui/core/Typography"
+// import Button from "@material-ui/core/Button"
+// import { Link } from "gatsby"
+// // import AudioAutoPlay from "../../components/audio-autoplay"
+
+// import sloganImg2 from "../images/slogan_eng2.png"
+// import { StaticQuery, graphql } from "gatsby"
+
+// const IndexPage = () => (
+//   <Layout>
+//     <div style={{ backgroundColor: "tomato" }}>
+//       <h1>Place for Article is HERE</h1>
+//       <StaticQuery
+//         query={graphql`
+//           {
+//             contentfulArticle {
+//               title
+//               text {
+//                 text
+//               }
+//             }
+//           }
+//         `}
+//         render={data => (
+//           <div style={{}}>
+//             <h1>{data.contentfulArticle.title}</h1>
+//             <p>{data.contentfulArticle.text.text}</p>
+//           </div>
+//         )}
+//       />
+//     </div>
+//   </Layout>
+// )
+
+// export default IndexPage
+
+// import React from "react"
+
+// const Article = ({
+//   content: {
+//     title,
+//     text: { text },
+//   },
+// }) => (
+//   <div>
+//     <h2>{title}</h2>
+//     <p>{text}</p>
+//   </div>
+// )
+
+// export default Article
